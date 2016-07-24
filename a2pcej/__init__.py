ae6b57d3c7a619fc2e3a677e9d9f788da15f58b8
Katsuya SAITO <hello@skatsuya.com>
2016-07-24 22:48:40 +0900

    Support command line interface.
    * README document is not updated

--- a2pcej/__init__.py
+++ a2pcej/__init__.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, unicode_literals
-from sys import version_info
+from sys import version_info, exit
+import argparse
 from .a2pcej import *
 """a2pcej, convert Alphabet to Phonetic Code in English and Japanease.
 
@@ -10,3 +11,48 @@ from .a2pcej import *
 __all__ = ['conv_al', 'conv_ak']
 if version_info[0] == 2:
     __all__ = [n.encode('ascii') for n in __all__]
+
+
+def main():
+    p = argparse.ArgumentParser()
+    p.add_argument('-m', '--mode', choices=['en', 'ja'], required=True)
+    p.add_argument('-d', '--delimiter')
+    p.add_argument('-nd', '--nodelimiter', action='store_true')
+    p.add_argument('-s', '--sign')
+    p.add_argument('-ns', '--nosign', action='store_true')
+    p.add_argument('-n', '--num', action='store_true')
+    p.add_argument('letters', nargs='+', type=str)
+
+    args = p.parse_args()
+
+    if args.mode == 'en':
+        cmd = conv_al
+    else:
+        cmd = conv_ak
+
+    if args.delimiter == None and not args.nodelimiter:
+        if args.mode == 'en':
+            delimiter = '-'
+        else:
+            delimiter = '・'
+    elif args.nodelimiter:
+        delimiter = ''
+    else:
+        delimiter = args.delimiter
+
+    if args.sign == None and not args.nosign:
+        if args.mode == 'en':
+            sign = '(CAPS)'
+        else:
+            sign = '（大文字）'
+    elif args.nosign:
+        sign = ''
+    else:
+        sign = args.sign
+
+    for s in args.letters:
+        print(cmd(s, delimiter=delimiter, sign=sign, num=args.num))
+
+
+if __name__ == '__main__':
+    exit(main())
